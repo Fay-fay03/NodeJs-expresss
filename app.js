e73e6f9f8c392c342be6e1dff05cfae7fba4fdc9
PC <mmusyoka354@gmail.com>
2024-05-13 09:38:06 +0300

    adding a new folder and http_module

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
-const http = require('http')
-const server = http.createServer((req,res) =>{
-    if(req.url === '/'){
-        res.end('This is our website')
-    }
-    else if (req.url === '/about'){
-        res.end('This is our short history')
-    }
-    else{
-        res.end(`
-        <h1>OOPS!</h1>
-        <p>We cannot find the page youre looking for</p>
-        <a href ="/">Back Home</a>
-        `)
-    }
-})
-server.listen(5000)
+const _ = require ("lodash")
 
-// const http = require('http')
-
-// const server = http.createServer((req,res) =>{
-
-//     if(req.url === '/'){
-//         res.end('Welcome to our website')
-//     }
-//     else if(req.url === '/about'){
-//         res.end('This is our short history')
-//     }
-//     else{res.end(`
-//     <h1>OOPS!</h1>
-//     <p>We can't seem to find the page youre looking for</p>
-//     <a href="/">back home</a>
-//     `)}
-// })
-
-// server.listen(5000)
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
+console.log("Hello World!!")
